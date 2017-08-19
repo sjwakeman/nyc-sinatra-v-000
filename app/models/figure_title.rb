131217c58b33b01a2e8591e5431a85971b83c3d1
Scott Wakeman <swakeman@ca.rr.com>
2017-08-19 22:45:40 +0000

    Done.

---
+++ app/models/figure_title.rb
@@ -0,0 +1,4 @@
+class FigureTitle < ActiveRecord::Base
+  belongs_to :sfigure
+  belongs_to :title
+end
---
+++ db/migrate/20170819215404_create_titles.rb
@@ -0,0 +1,7 @@
+class CreateTitles < ActiveRecord::Migration
+  def change
+    create_table :landmarks do |t|
+      t.string :name
+    end
+  end
+end
