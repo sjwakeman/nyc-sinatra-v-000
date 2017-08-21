767fcaa4e5a94fa83e2787e6f37e199864c195a8
Scott Wakeman <swakeman@ca.rr.com>
2017-08-21 20:25:49 +0000

    Done.

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,16 @@
+class FiguresController < ApplicationController
+
+  get '/figures/new' do
+    erb :"figures/new"
+  end
+
+  get "/figures" do
+    @figures = Figure.all
+    erb :"/figures/index"
+  end
+
+  get "/figures/:slug" do
+    @figure = Figure.find_by_slug(params["slug"])
+    erb :"/figures/show"
+  end
+end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,40 @@
 class LandmarksController < ApplicationController
 
-end
+  get '/landmarks' do
+     @landmarks = Landmark.all
+     erb :'landmarks/index'
+   end
+
+   get '/landmarks/new' do
+     erb :'landmarks/new'
+   end
+
+   post '/landmarks' do
+     @landmark = Landmark.create(:name => params["Name"])
+     @landmark.figure = Figure.find_or_create_by(:name => params["Figure Name"])
+     @landmark.title_ids = params[:titles]
+     @landmark.save
+     flash[:message] = "Successfully created landmark."
+     redirect("landmarks/#{@landmark.slug}")
+   end
+
+   get '/landmarks/:slug' do
+     @landmark = Landmark.find_by_slug(params[:slug]) #["slug"]
+     erb :'landmarks/show'
+   end
+
+   get '/landmarks/:slug/edit' do
+     @landmark = Landmarks.find_by_slug(params[:slug])
+     erb :'landmarks/edit'
+   end
+
+   patch '/landmarks/:slug' do
+
+     @landmark = Landmarks.find_by_slug(params[:slug])
+     @landmark.save(params[:landmark])
+     @landmark.figure = Figures.find_or_create_by(name: params[:figure][:name])
+     @landmark.save
+     flash[:message] = "Successfully updated landmark."
+     erb :'landmarks/show'
+   end
+ end
