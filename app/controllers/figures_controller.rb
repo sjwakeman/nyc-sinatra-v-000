992cb9c778a36d0265b514a6961ed22f459f0798
Scott Wakeman <swakeman@ca.rr.com>
2017-08-22 22:46:11 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,25 +1,45 @@
 class FiguresController < ApplicationController
 
-  get '/figures/new' do
-    erb :"figures/new"
-  end
-
-  post '/figures' do
-    @figure = Figure.create(:name => params["Name"])
-    @figure.landmark = Landmark.find_or_create_by(:name => params["Landmark Name"])
-    @figure.title_ids = params[:titles]
-    @figure.save
-    flash[:message] = "Successfully created figure."
-    redirect("figures/#{@figure.id}") #("gigures/#{@figure.slug}")
-  end
-
-  get "/figures" do
+  get '/figures' do
     @figures = Figure.all
     erb :"/figures/index"
   end
 
-  get "/figures/:id" do
-    @figure = Figure(params[:id]) #igure.find_by_slug(params["slug"])
-    erb :"/figures/show"
+  get '/figures/new' do
+    erb :"/figures/new"
   end
+
+  post "/figures/new" do
+    Figure.find_or_create_by(params["figure"])
+    erb :"/figures"
+  end
+
+  post '/figures' do
+    @figure = Figure.new(params[:figure])
+    unless params[:title][:name].empty?
+      @title = Title.create(params[:title])
+      @figure.titles << @title
+    end
+    unless params[:landmark][:name].empty?
+      @landmark = Landmark.create(params[:landmark])
+      @figure.landmarks << @landmark
+    end
+    @figure.save
+    erb :'/figures'
+  end
+
+  #post '/figures' do
+    #@figure = Figure.create(:name => params["Name"])
+    #@figure.landmark = Landmark.find_or_create_by(:name => params["Landmark Name"])
+    #@figure.title_ids = params[:title]
+    #@figure.save
+    #flash[:message] = "Successfully created figure."
+    #redirect("figures/#{@figure.id}") #("gigures/#{@figure.slug}")
+  #end
+
+  #get "/figures/:id" do
+    #@figure = Figure(params[:id]) #igure.find_by_slug(params["slug"])
+  #erb :"/figures/show"
+  #end
+
 end
