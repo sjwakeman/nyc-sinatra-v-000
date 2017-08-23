4711a33c85cdc9959e2f1b7701f68bb0dacf3692
Scott Wakeman <swakeman@ca.rr.com>
2017-08-23 00:51:51 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -25,21 +25,12 @@ class FiguresController < ApplicationController
       @figure.landmarks << @landmark
     end
     @figure.save
-    erb :'/figures'
+    erb :"/figures/#{@figure.id}"
   end
 
-  #post '/figures' do
-    #@figure = Figure.create(:name => params["Name"])
-    #@figure.landmark = Landmark.find_or_create_by(:name => params["Landmark Name"])
-    #@figure.title_ids = params[:title]
-    #@figure.save
-    #flash[:message] = "Successfully created figure."
-    #redirect("figures/#{@figure.id}") #("gigures/#{@figure.slug}")
-  #end
-
-  #get "/figures/:id" do
-    #@figure = Figure(params[:id]) #igure.find_by_slug(params["slug"])
-  #erb :"/figures/show"
-  #end
+  get '/figures/:id' do
+    erb :'figures/show'
+    @figure = Figure.find(params[:id])
+  end
 
 end
